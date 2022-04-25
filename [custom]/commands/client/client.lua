88542e9f9ffa4b95c652a29e66b8cd9a47101e67
WeebJesus <unconfigured@null.spigotmc.org>
2022-04-25 06:02:10 -0700

    patch

---
+++ [custom]/commands/client/client.lua
@@ -0,0 +1,12 @@
+registerCommand('ghelp', function ()
+    TriggerEvent('chatMessage', {
+        color = { 255, 0, 0},
+        multiline = true,
+        args = {"server", "Use /Report or join the Discord for help! https://discord.gg/rkufxUqxvQ"}
+      })
+      
+    })
+end) -- /ghelp
+
+
+-- [Server] : Use /Report or join the Discord for help! https://discord.gg/rkufxUqxvQ
---
+++ [custom]/commands/fxmanifest.lua
@@ -0,0 +1,9 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'MystrGee'
+description 'Chat System'
+version '0.0.1'
+
+client_script './client/client.lua'
